e445cf6f6bd087e7214a3460829f9b2840d45ab4
Thomas823 <35612259+Thomas823@users.noreply.github.com>
2018-04-25 12:56:05 -0400

    Final

--- TooManyStudentsThomasA/TooManyStudentsForm.Designer.cs
+++ TooManyStudentsThomasA/TooManyStudentsForm.Designer.cs
@@ -28,20 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblInputPrompt = new System.Windows.Forms.Label();
+            this.txtNumStudents = new System.Windows.Forms.TextBox();
+            this.btnCheckSize = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblInputPrompt
+            // 
+            this.lblInputPrompt.AutoSize = true;
+            this.lblInputPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInputPrompt.Location = new System.Drawing.Point(23, 67);
+            this.lblInputPrompt.Name = "lblInputPrompt";
+            this.lblInputPrompt.Size = new System.Drawing.Size(286, 20);
+            this.lblInputPrompt.TabIndex = 0;
+            this.lblInputPrompt.Text = "Enter the number of students (max 24):";
+            // 
+            // txtNumStudents
+            // 
+            this.txtNumStudents.Location = new System.Drawing.Point(331, 67);
+            this.txtNumStudents.Name = "txtNumStudents";
+            this.txtNumStudents.Size = new System.Drawing.Size(100, 20);
+            this.txtNumStudents.TabIndex = 1;
+            // 
+            // btnCheckSize
+            // 
+            this.btnCheckSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheckSize.Location = new System.Drawing.Point(161, 125);
+            this.btnCheckSize.Name = "btnCheckSize";
+            this.btnCheckSize.Size = new System.Drawing.Size(101, 31);
+            this.btnCheckSize.TabIndex = 2;
+            this.btnCheckSize.Text = "Check Size";
+            this.btnCheckSize.UseVisualStyleBackColor = true;
+            this.btnCheckSize.Click += new System.EventHandler(this.btnCheckSize_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(24, 193);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 20);
+            this.lblResult.TabIndex = 3;
             // 
             // frmTooManyStudents
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 262);
+            this.ClientSize = new System.Drawing.Size(491, 230);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCheckSize);
+            this.Controls.Add(this.txtNumStudents);
+            this.Controls.Add(this.lblInputPrompt);
             this.Name = "frmTooManyStudents";
             this.Text = "Too Many Students By Thomas Aubin";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInputPrompt;
+        private System.Windows.Forms.TextBox txtNumStudents;
+        private System.Windows.Forms.Button btnCheckSize;
+        private System.Windows.Forms.Label lblResult;
     }
 }
 
--- TooManyStudentsThomasA/TooManyStudentsForm.cs
+++ TooManyStudentsThomasA/TooManyStudentsForm.cs
@@ -12,9 +12,32 @@ namespace TooManyStudentsThomasA
 {
     public partial class frmTooManyStudents : Form
     {
+        // Declare constants and variables
+        const int maxStudents = 24;
+        int numStudents;
+
         public frmTooManyStudents()
         {
             InitializeComponent();
+        }
+
+        // Display text when the button is pressed
+        private void btnCheckSize_Click(object sender, EventArgs e)
+        {
+            // Get the number of students from the text box. Convert from string to a 32 bit integer
+            numStudents = Convert.ToInt32(txtNumStudents.Text);
+
+            // If number of students is greater then 24
+            if (numStudents > 24)
+            {
+                // Display text to inform user they have too many students
+                lblResult.Text = "You entered too many students";
+            }
+            else
+            {
+                // Display text to inform user they have the right amount of students
+                lblResult.Text = "Just the right amount of students";
+            }
         }
     }
 }
